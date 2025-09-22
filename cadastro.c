ab64140a1d8effa406cf27dbf171a37517435616
Louzeiro <louzeirosg@gmail.com>
2025-09-21 23:17:17 -0300

    Refatora cadastro de cartas e adiciona cálculo de Super Poder
    - Altera tipos de estado e código de char para string para permitir mais de uma letra.
    - Modifica tipo de população de int para unsigned long int para suportar grandes valores.
    - Adiciona novo atributo 'superPoder' e implementa cálculo baseado nos atributos da carta.
    - Ajusta scanf e printf para refletir alterações de tipo.
    - Corrige cálculos de densidade populacional e PIB per capita.

--- cadastro.c
+++ cadastro.c
@@ -4,37 +4,38 @@ int main()
 {
 
     // --------Carta 01--------
-    char estado1;                                 // recebe a letra referente ao estado
+    char estado1[5];                              // recebe a letra referente ao estado // alterei de char para string para receber mais de uma letra
     char codigo1[10];                             // recebe o codigo da carta
     char cidade1[50];                             // recebe o nome da cidade
-    int populacao1;                               // recebe o numero da populacao
+    unsigned long int populacao1;                 // recebe o numero da populacao trocado para unsigned long int era int
     float area1, pib1, densidade1, pibpercapita1; // recebe o valor da area, pib, densidade e pib per capita
+    float superPoder1;                            // novo atributo super poder
     int pontosturisticos1;                        // recebe o numero de pontos turisticos
 
     // ---------Carta 02--------
 
-    char estado2;                                 // recebe a letra referente ao estado
+    char estado2[5];                              // recebe a letra referente ao estado // alterei de char para string para receber mais de uma letra
     char codigo2[10];                             // recebe o codigo da carta
     char cidade2[50];                             // recebe o nome da cidade
-    int populacao2;                               // recebe o numero da populacao
+    unsigned long int populacao2;                 // recebe o numero da populacao trocado para unsigned long int era int
     float area2, pib2, densidade2, pibpercapita2; // recebe o valor da area, pib, densidade e pib per capita
+    float superPoder2;                            // novo atributo super poder
     int pontosturisticos2;                        // recebe o numero de pontos turisticos
 
     //----------Entrada de dados da primeira carta 1-----------------
 
     printf("\n--Cadastro da Carta A:--\n"); // cabeçalho do cadastro da carta 1
 
-    printf("Estado: \n"); // entrada do estado
-    scanf(" %c", &estado1);
+    printf("Estado: \n");   // entrada do estado
+    scanf(" %s", &estado1); // troquei %c por %s para receber mais de uma letra
 
     printf("Código da Carta A:\n"); // entrada do codigo da carta
-    scanf(" %s", &codigo1); 
-
+    scanf(" %s", &codigo1);
     printf("Nome da cidade:\n"); // entrada do nome da cidade
     scanf(" %s", &cidade1);
 
-    printf("População:\n"); // entrada da população
-    scanf(" %d", &populacao1);
+    printf("População:\n");     // entrada da população
+    scanf(" %lu", &populacao1); // troquei %d por %lu para unsigned long int
 
     printf("Área km²:\n"); // entrada da área
     scanf(" %f", &area1);
@@ -47,10 +48,10 @@ int main()
 
     // ------------Entrada de dados da segunda carta 2
 
-    printf("\n||Cadastro da carta B||\n"); // cabeçalho do cadastro da carta 2
+    printf("\n--Cadastro da carta B--\n"); // cabeçalho do cadastro da carta 2
 
-    printf("Estado:\n"); // entrada do estado
-    scanf(" %c", &estado2);
+    printf("Estado:\n");    // entrada do estado
+    scanf(" %s", &estado2); // troquei %c por %s para receber mais de uma letra
 
     printf("Código da carta B\n"); // entrada do codigo da carta
     scanf(" %s", &codigo2);
@@ -58,8 +59,8 @@ int main()
     printf("Nome da Cidade:\n"); // entrada do nome da cidade
     scanf(" %s", &cidade2);
 
-    printf("População:\n"); // entrada da população
-    scanf(" %d", &populacao2);
+    printf("População:\n");     // entrada da população
+    scanf(" %lu", &populacao2); // troquei %d por %lu para unsigned long int
 
     printf("Área:\n"); // entrada da área
     scanf(" %f", &area2);
@@ -73,16 +74,18 @@ int main()
     // Cálculos da Carta 1 e Carta 2
     densidade1 = populacao1 / area1;
     pibpercapita1 = pib1 / populacao1;
+    superPoder1 = (float)populacao1 + area1 + pib1 + pontosturisticos1 + pibpercapita1 + 1 / densidade1 + pontosturisticos1; // cálculo do super poder da carta 1
 
     densidade2 = populacao2 / area2;
     pibpercapita2 = pib2 / populacao2;
+    superPoder2 = (float)populacao2 + area2 + pib2 + pontosturisticos2 + pibpercapita2 + 1 / densidade2 + pontosturisticos2; // cálculo do super poder da carta 2
 
     // saida da carta 1
     printf("\n--Carta A--\n");                                      // cabeçalho da carta 1
-    printf("Estado: %c\n", estado1);                                // estado da carta
+    printf("Estado: %s\n", estado1);                                // estado da carta // troquei %c por %s para receber mais de uma letra
     printf("Código: %s\n", codigo1);                                // codigo da carta
     printf("Nome da Cidade: %s\n", cidade1);                        // nome da cidade
-    printf("População: %d\n", populacao1);                          // população
+    printf("População: %lu\n", populacao1);                         // população // troquei %d por %lu para unsigned long int
     printf("Área: %.2f km²\n", area1);                              // área da cidade
     printf("PIB: %.2f bilhões de reais\n", pib1);                   // pib da cidade
     printf("Número de Pontos Turísticos: %d\n", pontosturisticos1); // numero de pontos turisticos
@@ -92,15 +95,25 @@ int main()
 
     // saida da carta 2
     printf("\n--Carta B--\n");                                      // cabeçalho da carta 2
-    printf("Estado: %c\n", estado2);                                // estado da carta
+    printf("Estado: %s\n", estado2);                                // estado da carta // troquei %c por %s para receber mais de uma letra
     printf("Código: %s\n", codigo2);                                // codigo da carta
     printf("Nome da Cidade: %s\n", cidade2);                        // nome da cidade
-    printf("População: %d\n", populacao2);                          // população
+    printf("População: %lu\n", populacao2);                         // população // troquei %d por %lu para unsigned long int
     printf("Área: %.2f km²\n", area2);                              // área da cidade
     printf("PIB: %.2f bilhões de reais\n", pib2);                   // pib da cidade
     printf("Número de Pontos Turísticos: %d\n", pontosturisticos2); // numero de pontos turisticos
     printf("Densidade Populacional: %.2f hab/km²\n", densidade2);   // densidade populacional
     printf("PIB per Capita: %.2f reais\n", pibpercapita2);          // pib per capita
+
+    // --- Comparação das Cartas e Exibição dos Resultados ---
+    printf("\n\n--- RESULTADOS DAS COMPARAÇÕES ---\n");
+    printf("População: Carta A venceu (%d)\n", populacao1 > populacao2);                       // compara e exibe o resultado da comparação de população
+    printf("Área: Carta A venceu (%d)\n", area1 > area2);                                      // compara e exibe o resultado da comparação de área
+    printf("PIB: Carta A venceu (%d)\n", pib1 > pib2);                                         // compara e exibe o resultado da comparação de pib
+    printf("Pontos Turísticos: Carta A venceu (%d)\n", pontosturisticos1 > pontosturisticos2); // compara e exibe o resultado da comparação de pontos turísticos
+    printf("Densidade Populacional: Carta A venceu (%d)\n", densidade1 < densidade2);          // compara e exibe o resultado da comparação de densidade populacional
+    printf("PIB per Capita: Carta A venceu (%d)\n", pibpercapita1 > pibpercapita2);            // compara e exibe o resultado da comparação de pib per capita
+    printf("Super Poder: Carta A venceu (%d)\n", superPoder1 > superPoder2);                   // compara e exibe o resultado da comparação de super poder
 
     return 0;
 }
